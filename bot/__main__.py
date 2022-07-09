91efe5298b784f352565471b4d5dd8588fc2bf8b
Dark Lord <106424420+DarkLordlk@users.noreply.github.com>
2022-07-09 16:34:24 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,8 +54,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("⚡️Owner⚡️", "https://t.me/Dark_LordSl")
+    buttons.buildbutton("🧲Group🧲", "https://t.me/+3RufWFb8DOgyYTk9")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
